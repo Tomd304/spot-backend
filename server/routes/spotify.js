f736dee5fdd947185e28c98364d967c5c572c272
Tomd304 <tomd304@live.co.uk>
2022-05-17 14:19:22 +0100

    Route and controller added to handle get, save and remove liked songs

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -4,14 +4,20 @@ var controller = require("../controllers/spotify_controller");
 
 router.get("/getSavedAlbums", controller.getSavedAlbums);
 
-// router.get("/getPlaylists", controller.getPlaylists);
-
 router.put("/saveAlbum", controller.saveAlbum);
 
 router.delete("/removeAlbum", controller.removeAlbum);
 
+// router.get("/getPlaylists", controller.getPlaylists);
+
 // router.put("/createPlaylist", controller.createPlaylist);
 
 // router.put("/addPlaylistTracks", controller.addPlaylistTracks);
+
+router.get("/getSavedTracks", controller.getSavedTracks);
+
+router.put("/saveTrack", controller.saveTrack);
+
+router.delete("/removeTrack", controller.removeTrack);
 
 module.exports = router;
